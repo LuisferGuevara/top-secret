8e60be5fe7bb3825348386ff81b7fac21b5c87d3
Luisfer Guevara <luisferguevaraa@gmail.com>
2023-02-02 11:39:13 +0100

    FEAT: styled questionary

--- src/ui/Dashboard/Admin/Editor/QuestionList.jsx
+++ src/ui/Dashboard/Admin/Editor/QuestionList.jsx
@@ -1,14 +1,15 @@
 import { TriangleDownIcon } from '@chakra-ui/icons';
-import { Box, Button, Divider, Flex, Heading, Text, Icon } from '@chakra-ui/react';
+import { Box, Button, Divider, Flex, Heading, Text, Icon, Stack } from '@chakra-ui/react';
 import { useState } from 'react';
 import { data } from '../../../../tests';
 import Question from './Question';
 
 const algo = Object.values(data[0]['CIENCIAS JURIDICAS'][0])[0];
 
-
 const testFromServer = [...Object.values(algo[0])].pop();
+
 let array = Object.keys;
+
 const QuestionLists = () => {
   return (
     <div>
@@ -19,12 +20,24 @@ const QuestionLists = () => {
           </Heading>
           <Divider border="2px solid #37496A" mb="10px" />
           {category[array(category)[0]].map(unit => (
-            <Flex w="100%" justifyContent={'space-between'}>
-              <Text py="15px">{array(unit)[0]}</Text>
-              <Button>
-                <p>Ver</p>
-                <Icon as={TriangleDownIcon} mx="5px"/>
-              </Button>
+            <Flex w="100%" justifyContent={'space-between'} flexDirection="column">
+              <Flex w="100%" justifyContent={'space-between'}>
+                <Text my="5x" py="15px" textAlign={'start'} maxW="80%">
+                  {array(unit)[0]}
+                </Text>
+                <Button my="10px">
+                  <p>Ver</p>
+                  <Icon as={TriangleDownIcon} mx="2px" />
+                </Button>
+              </Flex>
+              <Flex bg="blue.200" flexDirection={'column'} align="start" px="5px" py="5px" borderRadius={'14px'}>
+                {unit[array(unit)[0]].map(test => (
+                  <Flex justifyContent={'space-between'} w="100%" my="8px">
+                    <Text py="6px">{array(test)[0]}</Text>
+                    <Button>Editar</Button>
+                  </Flex>
+                ))}
+              </Flex>
             </Flex>
           ))}
         </Flex>
