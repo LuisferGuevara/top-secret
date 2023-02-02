173d936201c5662f0cd68b76a4f11f9c42f310d1
Luisfer Guevara <luisferguevaraa@gmail.com>
2023-02-02 19:07:13 +0100

    :)

--- src/ui/Dashboard/Admin/Card.jsx
+++ src/ui/Dashboard/Admin/Card.jsx
@@ -2,7 +2,7 @@ import { Box } from '@chakra-ui/react';
 import RenderEditor from './Editor/RenderEditor';
 
 const Cartita = props => (
-  <Box minH="3xs" maxH="86.8vh" overflow="scroll" bg="bg-surface" boxShadow="sm" borderRadius="lg" {...props}>
+  <Box minH="3xs" maxH="95vh" overflow="scroll" bg="bg-surface" boxShadow="sm" borderRadius="lg" {...props}>
     <RenderEditor />
   </Box>
 );
